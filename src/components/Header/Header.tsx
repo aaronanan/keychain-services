d29215b47453f9c6414549e30029190048823e75
harishanan <harish7673@hotmail.com>
2022-03-19 23:56:27 -0700

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from 'styles/Header.module.css';
+import { VscAccount } from 'react-icons/vsc';
 
 const Header: React.FC = () => (
   <div className={styles.container}>
-    <h1>Hello</h1>
+    <div className={styles.header}>
+      <h1 className={styles.logo}>{process.env.titleName}</h1>
+      <VscAccount size={25} className={styles.profile} />
+    </div>
   </div>
 );
 
