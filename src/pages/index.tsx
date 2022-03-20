7d416fcb321c4d4729655e9bd6695f58d8968801
harishanan <harish7673@hotmail.com>
2022-03-19 20:17:02 -0700

    added prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage, NextApiHandler } from 'next';
+import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
@@ -13,14 +13,11 @@ const Home: NextPage = () => (
 
     <main className={styles.main}>
       <h1 className={styles.title}>
-        Welcome to
-        {' '}
-        <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className={styles.description}>
-        Get started by editing
-        {' '}
+        Get started by editing{' '}
         <code className={styles.code}>pages/index.tsx</code>
       </p>
 
@@ -37,20 +34,16 @@ const Home: NextPage = () => (
 
         <a
           href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
+          className={styles.card}>
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
+          className={styles.card}>
           <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
       </div>
     </main>
@@ -59,10 +52,8 @@ const Home: NextPage = () => (
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by
-        {' '}
+        rel="noopener noreferrer">
+        Powered by{' '}
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
